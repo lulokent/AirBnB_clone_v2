54798b3d91455073818fd9a593eb0815caaa787a
lulokent <cwengalulonkem@gmail.com>
2024-05-22 20:55:11 -0700

    A few changes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,19 +5,19 @@ from flask import render_template
 web_app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
-def flask_greeting():
+@w_app.route('/', strict_slashes=False)
+def landing_page():
     """ Display greeting """
     return "Hello HBNB!"
 
 
-@web_app.route('/hbnb', strict_slashes=False)
-def flask_greeting2():
+@w_app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """ Display greeting"""
     return "HBNB"
 
 
-@web_app.route("/c/<text>", strict_slashes=False)
+@w_app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """ formated text returned as a string"""
     return 'C ' + text.replace('_', ' ')
@@ -30,23 +30,23 @@ def py_greeting(text="is cool"):
     return "Python " + text.replace("_", " ")
 
 
-@web_app.route("/number/<int:n>", strict_slashes=False)
-def num_display(n):
+@w_app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
     """ returns 'n is a number' is n is integer """
     return "{} is a number".format(n)
 
 
-@web_app.route("/number_template/<int:n>", strict_slashes=False)
+@w_app.route("/number_template/<int:n>", strict_slashes=False)
 def num_template(n):
     """ returns an html page if n is an integer """
     return render_template("5-number.html", n=n)
 
 
-@web_app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@w_app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_or_even(n):
     """ Displays an html page if n is integer, also displays if even or odd """
     return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
-    web_app.run('0.0.0.0')
+    w_app.run(host='0.0.0.0', port=5000)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,21 +3,21 @@
 from flask import Flask, render_template
 from models.state import State
 from models import storage
-web_app = Flask(__name__)
+w_app = Flask(__name__)
 
 
-@web_app.route("/states_list", strict_slashes=False)
+@w_app.route("/states_list", strict_slashes=False)
 def states_list():
     """ loads template states"""
     state_list = storage.all(State)
     return render_template("7-states_list.html", states=state_list)
 
 
-@web_app.teardown_appcontext
+@w_app.teardown_appcontext
 def teardown(arg=None):
     """ Close current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    web_app.run(host="0.0.0.0")
+    w_app.run(host="0.0.0.0", port=5000)
