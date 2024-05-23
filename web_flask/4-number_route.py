b63b67f80fc0625e41bc824d19dbc8f8ad3a0ca2
lulokent <cwengalulonkem@gmail.com>
2024-05-22 19:18:33 -0700

    task 3-5

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
 from flask import Flask
-web_app = Flask(__name__)
+w_app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
-def flask_greeting():
-    """ Display greeting """
+@w_app.route('/', strict_slashes=False)
+def landing_page():
+    """ Displays greeting on home page."""
     return "Hello HBNB!"
 
 
-@web_app.route('/hbnb', strict_slashes=False)
+@w_app.route('/hbnb', strict_slashes=False)
 def flask_greeting2():
-    """ Display greeting"""
+    """ Displays a greeting"""
     return "HBNB"
 
 
-@web_app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """ formated text returned as a string"""
+@w_app.route("/c/<text>", strict_slashes=False)
+def c_with_text(text):
+    """ Displays C followed by a string."""
     return 'C ' + text.replace('_', ' ')
 
 
-@web_app.route("/python/", strict_slashes=False)
-@web_app.route("/python/<text>", strict_slashes=False)
-def py_greeting(text="is cool"):
+@w_app.route('/python/', defaults={'text':'is_cool'})
+@w_app.route('/python/<text>')
+def python_text_params(text):
     """ Displays python followed by value of text """
     return "Python " + text.replace("_", " ")
 
 
-@web_app.route("/number/<int:n>", strict_slashes=False)
+@w_app.route('/number/<int:n>')
 def num_display(n):
     """ returns 'n is a number' is n is integer """
     return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
-    web_app.run('0.0.0.0')
+    w_app.run(host='0.0.0.0', port=5000)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,45 +2,45 @@
 """ Starts a Flask web application """
 from flask import Flask
 from flask import render_template
-web_app = Flask(__name__)
+w_app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
-def flask_greeting():
-    """ Display greeting """
+@w_app.route('/', strict_slashes=False)
+def landing_page():
+    """ Displays greeting on the home page"""
     return "Hello HBNB!"
 
 
-@web_app.route('/hbnb', strict_slashes=False)
-def flask_greeting2():
-    """ Display greeting"""
+@w_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays greeting"""
     return "HBNB"
 
 
-@web_app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
+@w_app.route("/c/<text>", strict_slashes=False)
+def c_with_text(text):
     """ formated text returned as a string"""
     return 'C ' + text.replace('_', ' ')
 
 
-@web_app.route("/python/", strict_slashes=False)
-@web_app.route("/python/<text>", strict_slashes=False)
-def py_greeting(text="is cool"):
+@w_app.route('/python/', defaults={'text': 'is_cool'})
+@w_app.route('/python/<text>')
+def python_text_params(text):
     """ Displays python followed by value of text """
     return "Python " + text.replace("_", " ")
 
 
-@web_app.route("/number/<int:n>", strict_slashes=False)
-def num_display(n):
+@w_app.route('/number/<int:n>')
+def number(n):
     """ returns 'n is a number' is n is integer """
     return "{} is a number".format(n)
 
 
-@web_app.route("/number_template/<int:n>", strict_slashes=False)
-def num_template(n):
+@w_app.route('/number_template/<int:n>')
+def number_template(n):
     """ returns an html page if n is an integer """
-    return render_template("5-number.html", n=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
-    web_app.run('0.0.0.0')
+    w_app.run(host='0.0.0.0', port=5000)
