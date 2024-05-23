25296ff9440544aede574c57731af388c30b5c7f
lulokent <cwengalulonkem@gmail.com>
2024-05-22 17:30:50 -0700

    tasks 1-2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
 from flask import Flask
-web_app = Flask(__name__)
+w_app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
-def flask_greeting():
-    """ Display greeting """
+@w_app.route('/', strict_slashes=False)
+def landing_page():
+    """ Display greeting on the home page."""
     return "Hello HBNB!"
 
 
-@web_app.route('/hbnb', strict_slashes=False)
-def flask_greeting2():
-    """ Display greeting"""
+@w_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays greeting."""
     return "HBNB"
 
 
 if __name__ == "__main__":
-    web_app.run('0.0.0.0')
+    web_app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
 from flask import Flask
-web_app = Flask(__name__)
+w_app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
-def flask_greeting():
-    """ Display greeting """
+@w_app.route('/', strict_slashes=False)
+def landing_page():
+    """ Display greeting on the home page."""
     return "Hello HBNB!"
 
 
-@web_app.route('/hbnb', strict_slashes=False)
-def flask_greeting2():
+@w_app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """ Display greeting"""
     return "HBNB"
 
 
-@web_app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """ formated text returned as a string"""
+@w_app.route("/c/<text>", strict_slashes=False)
+def c_with_text(text):
+    """ Displays C followed by a string."""
     return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
-    web_app.run('0.0.0.0')
+    w_app.run(host='0.0.0.0', port=5000)
