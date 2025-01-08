5e1be7161c799d084c811ce8a0cc860bf6e7c25e
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2025-01-08 16:02:40 +0200

    mysql test setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Creates a MySQL server with :
 -- Creates a DATABASE hbnb_test_db
-CREATE DATABASE IF NOT EXISTS Hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create a new user hbnb_test with password hbnb_test_pwd in localhost
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
@@ -11,4 +11,3 @@ FLUSH PRIVILEGES;
 
 -- GRANT SELECT privileges for hbnb_test on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
